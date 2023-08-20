faa97f37e6a64d7ce580c544f9eb961f632daeda
Rushil <ragarwal2_be21@thapar.edu>
2023-08-20 18:24:42 +0530

    key change

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,8 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyA6776Erq3uMBERYaGBfgZT9WuMF_3jMOA",
-    authDomain: "netflix-clone-6525f.firebaseapp.com",
-    projectId: "netflix-clone-6525f",
-    storageBucket: "netflix-clone-6525f.appspot.com",
-    messagingSenderId: "129923015162",
-    appId: "1:129923015162:web:1f2406febf825b0d4e4443"
-  };
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: "netflix-clone-6525f.firebaseapp.com",
+  projectId: "netflix-clone-6525f",
+  storageBucket: "netflix-clone-6525f.appspot.com",
+  messagingSenderId: "129923015162",
+  appId: "1:129923015162:web:1f2406febf825b0d4e4443",
+};
